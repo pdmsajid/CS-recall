36115c79b5dee56394c4cda17b177d2387eddf0d
Sajid Pakirdeen <pdmsajid@gmail.com>
2023-07-23 09:03:00 +1000

    Initial Commit

---
+++ CS-recall/CS_recallApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS_recallApp.swift
+//  CS-recall
+//
+//  Created by Sajee on 23/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct CS_recallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
